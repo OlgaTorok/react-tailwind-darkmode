b11a86e775a57ce649650a2d24289174bf9467f1
OlgaTorok <33686903+OlgaTorok@users.noreply.github.com>
2024-04-19 23:39:53 +0100

    Added quotes + fonts and colours

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,83 @@
 import './App.css';
 
 function App() {
+
   return (
-    <>
-      <div>
-        
+    <main className='flex h-screen w-full text-slate-700'>
+      <div className='flex flex-col mx-auto p-20 bg-slate-400'>
+        <div className='flex mx-auto pb-20'>
+          <h1 className='font-extrabold text-3xl px-4'>Famous Quotes</h1>
+        </div>
+
+        <div className='grid grid-cols-3 gap-4 md:gap-10 mx-auto'>
+          <div className='flex flex-col gap-6'>
+            <h1 className='font-bold text-xl text-center'>Famous People</h1>
+            <span className='quote p-4 text-center'>
+              The greatest glory in living lies not in never falling, but in
+              rising every time we fall.
+              <br />
+              <span className='font-sans text-sm'>Nelson Mandela</span>
+            </span>
+            <span className='quote p-4 text-center'>
+              Your time is limited, so don&apos;t waste it living someone
+              else&apos;s life. Don&apos;t be trapped by dogma – which is living
+              with the results of other people&apos;s thinking.
+              <br />
+              <span className='font-sans text-sm'>Steve Jobs</span>
+            </span>
+            <span className='quote p-4 text-center'>
+              You may say I&apos;m a dreamer, but I&apos;m not the only one. I
+              hope someday you&apos;ll join us. And the world will live as one.
+              <br />
+              <span className='font-sans text-sm'>John Lennon</span>
+            </span>
+          </div>
+
+          <div className='flex flex-col gap-6'>
+            <h1 className='font-bold text-xl text-center'>
+              Best Quotes of All Time
+            </h1>
+            <span className='quote p-4 text-center'>
+              Be yourself; everyone else is already taken.
+              <br />
+              <span className='font-sans text-sm'>Oscar Wilde</span>
+            </span>
+            <span className='quote p-4 text-center'>
+              You must be the change you wish to see in the world.
+              <br />
+              <span className='font-sans text-sm'>Mahatma Gandhi</span>
+            </span>
+            <span className='quote p-4 text-center'>
+              Do not go where the path may lead, go instead where there is no
+              path and leave a trail.
+              <br />
+              <span className='font-sans text-sm'>Ralph Waldo Emerson</span>
+            </span>
+          </div>
+
+          <div className='flex flex-col gap-6'>
+            <h1 className='font-bold text-xl text-center'>Success Quotes</h1>
+            <span className='quote p-4 text-center'>
+              Success usually comes to those who are too busy to be looking for
+              it.
+              <br />
+              <span className='font-sans text-sm'>Henry David Thoreau</span>
+            </span>
+            <span className='quote p-4 text-center'>
+              If you want to make your dreams come true, the first thing you
+              have to do is wake up.
+              <br />
+              <span className='font-sans text-sm'>J.M. Power</span>
+            </span>
+            <span className='quote p-4 text-center'>
+              The secret of success is to do the common thing uncommonly well.
+              <br />
+              <span className='font-sans text-sm'>John D. Rockefeller Jr.</span>
+            </span>
+          </div>
+        </div>
       </div>
-    </>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#fff',
+      slate: '#64748b',
+      lightSlate: '#e2e8f0',
+      darkSlate: '#1e293b',
+    },
+    fontFamily: {
+      sans: ['Raleway', 'sans-serif'],
+      cursive: ['Marck Script', 'cursive'],
+    },
     extend: {},
   },
   plugins: [],
